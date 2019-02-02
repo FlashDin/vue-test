9d004ae9f4bc7b4a22519b1986c01e3763d51452
flashdin <flashdin007@gmail.com>
2019-02-02 12:10:42 +0700

    first commit

---
+++ src/components/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <md-content>
+    <router-link to="/home">
+      <md-button class="md-button md-raised md-primary">
+        <i class="material-icons">android</i>
+        {{msg}}
+      </md-button>
+    </router-link>
+  </md-content>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      msg: 'Home'
+    }
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+// non individual component vue-material
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+// Vue.use(MdButton)
+// Vue.use(MdContent)
+// Vue.use(MdTabs)
+Vue.use(VueMaterial)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
